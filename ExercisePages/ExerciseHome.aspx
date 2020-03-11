dd59cd2c3f8717aed26ca901b85de959f9dfe72d
Jack Lee <jlee192@nait.ca>
2020-03-11 17:08:00 -0600

    more changes

--- ExercisePages/ExerciseHome.aspx
+++ ExercisePages/ExerciseHome.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ExerciseHome.aspx.cs" Inherits="WebAppFSIS.ExercisePages.WebForm1" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ExerciseHome.aspx.cs" Inherits="WebAppFSIS.ExercisePages.ExerciseHome" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
     <h1>Hey Man</h1>
 </asp:Content>
--- ExercisePages/SimpleQuery.aspx.cs
+++ ExercisePages/SimpleQuery.aspx.cs
@@ -5,11 +5,18 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using FSISSystem.BLL;
+using FSISSystem.ENTITIES;
+
 namespace WebAppFSIS.ExercisePages
 {
-    public partial class WebForm2 : System.Web.UI.Page
+    public partial class SimpleQuery : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
+        {
+            MessageLabel.Text = "";
+        }
+        protected void Fetch_Click(object sender, EventArgs e)
         {
 
         }
