7b435c3db15289bb939919a1f31d49b1984b69c8
Jack Lee <jlee192@nait.ca>
2020-03-11 16:50:53 -0600

    made more changes

---
+++ FSISSystem/BLL/TeamController.cs
@@ -0,0 +1,16 @@
+﻿using FSISSystem.DAL;
+using FSISSystem.ENTITIES;
+
+namespace FSISSystem.BLL
+{
+    public class TeamController
+    {
+        public Team Teams_FindByID(int teamid)
+        {
+            using (var context = new FSISContext())
+            {
+                return context.Teams.Find(teamid);
+            }
+        }
+    }
+}
